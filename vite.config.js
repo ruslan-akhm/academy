c80615cef771d3062b63f34b5fb66fa5c2913632
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2026-06-22 20:36:11 -0400

    added base property to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        host: true, // or host: '0.0.0.0'
-    },
+    // server: {
+    //     host: true, // or host: '0.0.0.0'
+    // },
+    base: "/",
 });
